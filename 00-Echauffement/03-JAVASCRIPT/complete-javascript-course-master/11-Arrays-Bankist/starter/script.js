a1aaf8417016c157ff77b46b04d537681f775641
DayOneAI <bambaopenbim@gmail.com>
2023-03-04 17:07:10 +0100

    Bankist1

--- 00-Echauffement/03-JAVASCRIPT/complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ 00-Echauffement/03-JAVASCRIPT/complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -61,6 +61,24 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = function (movements) {
+  movements.forEach((mov, i) => {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+    const html = `
+      <div class="movements__row">
+        <div class="movements__type movements__type--${type}">${
+      i + 1
+    } ${type}</div>
+        <div class="movements__value">${mov} €</div>
+      </div>
+    `;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+displayMovements(account1.movements);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -74,3 +92,71 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+let tab = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+// console.log(tab.reverse(3));
+// console.log(tab);
+// tab.splice(-1);
+// console.log(tab);
+// console.log(tab.slice(-2));
+// console.log(tab.slice(-1));
+// console.log(tab.slice());
+
+// concat
+let tab1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+let tab2 = ['h', 'i', 'j', 'k', 'l'];
+const tab3 = tab1.concat(tab2);
+// console.log(tab3);
+// console.log(tab1);
+// console.log(tab2);
+
+// join
+// console.log(tab3.join(' - '));
+// console.log(tab1);
+// console.log(tab2);
+
+// at
+const tab4 = [3, 65, 99];
+// console.log(tab4.at(1));
+// console.log(tab4[1]);
+// console.log(tab4.at(-1));
+// console.log('Bamba'.at(0));
+
+// forEach
+const tests = [10, -50, 4800, 2800, -7650, 5030, 960, 698];
+
+tests.forEach(element => {
+  //console.log(element);
+});
+
+movements.forEach(mov => {
+  if (mov > 0) {
+    //console.log(`You deposited ${mov}`);
+  } else {
+    //console.log(`You withdraw ${Math.abs(mov)}`);
+  }
+});
+
+movements.forEach((mov, i, array) => {
+  if (mov > 0) {
+    //console.log(`Test ${i + 1} : Bravo tu as gagné ${mov} point(s)`);
+  } else {
+    //console.log(`Test ${i + 1} : Oups, tu as perdu ${Math.abs(mov)} point(s)`);
+  }
+});
+
+const crypto = new Map([
+  ['BTC', 'Bitcoin'],
+  ['ETH', 'Etherium'],
+  ['USDT', 'Tether USD'],
+  ['BNB', 'Binance Coin'],
+]);
+
+crypto.forEach((value, key, map) => {
+  //console.log(`${key}: ${value}`);
+});
+
+const cryptoUnique = new Set(['BTC', 'BTC', 'BNB', 'USDT', 'USDT', 'ETH']);
+// console.log(cryptoUnique);
+cryptoUnique.forEach((value, _, map) => {
+  //console.log(`${value}: ${value}`);
+});
